58ebffbe3518b6f386837b4c82586a31a3efd640
Yf3rs0n <yferson5020@gmail.com>
2023-07-12 19:10:19 -0500

    Creación del proyecto

---
+++ src/test/java/com/api/nimble/NimbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.nimble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
